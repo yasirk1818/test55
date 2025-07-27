1f63e1ce31b9cc876e78184a47db2964ebc3d847
yasirk1818 <yasirk1818@gmail.com>
2025-07-27 22:19:28 +0100

    Create whatsappManager.js

---
+++ services/whatsappManager.js
@@ -0,0 +1,54 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const DeviceSession = require('../models/DeviceSession');
+
+const activeClients = {}; // { sessionId: client }
+
+function initClient(sessionId, userId, io) {
+  const client = new Client({
+    authStrategy: new LocalAuth({ clientId: sessionId }),
+    puppeteer: { headless: true, args: ['--no-sandbox'] }
+  });
+
+  client.on('qr', qr => {
+    io.to(userId.toString()).emit('qr', { qr, sessionId });
+  });
+
+  client.on('ready', async () => {
+    await DeviceSession.findByIdAndUpdate(sessionId, {
+      status: 'connected'
+    });
+    activeClients[sessionId] = client;
+    io.to(userId.toString()).emit('ready', { sessionId });
+  });
+
+  client.on('disconnected', async () => {
+    await DeviceSession.findByIdAndUpdate(sessionId, {
+      status: 'disconnected'
+    });
+    delete activeClients[sessionId];
+  });
+
+  client.initialize();
+}
+
+async function addNewDevice(userId, name, io) {
+  const session = new DeviceSession({ userId, name });
+  await session.save();
+  initClient(session._id.toString(), userId, io);
+  return session;
+}
+
+async function removeDevice(sessionId) {
+  const client = activeClients[sessionId];
+  if (client) {
+    await client.destroy();
+    delete activeClients[sessionId];
+  }
+  await DeviceSession.findByIdAndDelete(sessionId);
+}
+
+module.exports = {
+  initClient,
+  addNewDevice,
+  removeDevice
+};
